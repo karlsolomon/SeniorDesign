2b703492618a81b06c2ebaed9d4cbadbb20f4b76
karlsolomon <ksolomon@utexas.edu>
2017-11-28 20:09:10 -0600

    store result in text file

--- python-compare-two-images/compare_all.py
+++ python-compare-two-images/compare_all.py
@@ -1,5 +1,5 @@
 # USAGE
-# python untitled.py
+# python compare_all.py
 
 # import the necessary packages
 from skimage.measure import structural_similarity as ssim
@@ -19,20 +19,17 @@ def mse(imageA, imageB):
 	# the two images are
 	return err
 
-def print_difference(imageA, imageB):
-	m = mse(imageA, imageB)
-	s = ssim(imageA, imageB)
-	print("MSE:%.2f\tSSI%.2f" % (m,s))
-
-# load the images -- the original, the original + contrast,
-# and the original + photoshop
-
+# load all images in the images/ directory and compare each a & b versions of the files
 path, dirs, files = os.walk("images").next()
 file_count = len(files)
-file_count = file_count / 2
+file_count = file_count / 2 #a and b for each image
+file = open("comparison.txt", "w")
 for i in range(file_count):
 	a = cv2.imread("images/%sa.png" % (str(i+1)))
 	b = cv2.imread("images/%sb.png" % (str(i+1)))
 	a = cv2.cvtColor(a, cv2.COLOR_BGR2GRAY)
 	b = cv2.cvtColor(b, cv2.COLOR_BGR2GRAY)
-	print_difference(a, b)
+	m = mse(a, b)
+	s = ssim(a, b)
+	file.write("%s\tMSE:%.2f\tSSIM:%.2f\n" % (str(i+1),m,s))
+file.close()
