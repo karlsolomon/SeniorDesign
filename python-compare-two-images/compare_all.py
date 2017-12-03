b6bf352ff74eaffe6429d2991212eb6e9878f432
karlsolomon <ksolomon@utexas.edu>
2017-12-03 13:04:02 -0600

    use iOS naming convention

--- python-compare-two-images/compare_all.py
+++ python-compare-two-images/compare_all.py
@@ -8,6 +8,11 @@ import numpy as np
 import cv2
 import os
 
+def my_range(start, end, step):
+    while start <= end:
+        yield start
+        start += step
+
 def mse(imageA, imageB):
 	# the 'Mean Squared Error' between the two images is the
 	# sum of the squared difference between the two images;
@@ -19,17 +24,32 @@ def mse(imageA, imageB):
 	# the two images are
 	return err
 
-# load all images in the images/ directory and compare each a & b versions of the files
-path, dirs, files = os.walk("images").next()
-file_count = len(files)
-file_count = file_count / 2 #a and b for each image
-file = open("comparison.txt", "w")
-for i in range(file_count):
-	a = cv2.imread("images/%sa.png" % (str(i+1)))
-	b = cv2.imread("images/%sb.png" % (str(i+1)))
+def print_results(a, b, i):
 	a = cv2.cvtColor(a, cv2.COLOR_BGR2GRAY)
 	b = cv2.cvtColor(b, cv2.COLOR_BGR2GRAY)
 	m = mse(a, b)
 	s = ssim(a, b)
-	file.write("%s\tMSE:%.2f\tSSIM:%.2f\n" % (str(i+1),m,s))
+	file.write("%s\tMSE:%.2f\tSSIM:%.6f\n" % (str(i+1),m,s))
+
+
+
+# load all images in the images/ directory and compare each a & b versions of the files
+#path, dirs, files = os.walk("images").next()
+#file_count = len(files)
+#file_count = file_count / 2 #a and b for each image
+file = open("comparison.txt", "w")
+minNum = 702
+maxNum = 710
+for i in my_range(minNum,maxNum,2):
+	print(i)
+	if(i < 9):
+		a = cv2.imread("images/IMG_0%s.JPG" % (str(i)))
+		b = cv2.imread("images/IMG_0%s.JPG" % (str(i+1)))
+		print_results(a,b,i)
+	else:
+		a = cv2.imread("images/IMG_0%s.JPG" % (str(i)))
+		b = cv2.imread("images/IMG_0%s.JPG" % (str(i+1)))
+		print_results(a,b,i)
 file.close()
+
+
