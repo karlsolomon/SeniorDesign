4c54ad00e64220fe836861a852b3a6aedbdac172
karlsolomon <ksolomon@utexas.edu>
2017-12-03 16:49:48 -0600

    clean up

--- python-compare-two-images/compare_all.py
+++ python-compare-two-images/compare_all.py
@@ -21,9 +21,6 @@ def mse(imageA, imageB):
 	# NOTE: the two images must have the same dimension
 	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
 	err /= float(imageA.shape[0] * imageA.shape[1])
-	
-	# return the MSE, the lower the error, the more "similar"
-	# the two images are
 	return err
 
 def print_results(a, b, i):
@@ -45,12 +42,7 @@ def diff_polarize(a,b,i):
 	c = np.uint8(c)
 	c = 255-c
 	cv2.imwrite("images/will/diff%s.jpeg" % (str(i)), c) #Write image to file
-	
-# load all images in the images/ directory and compare each a & b versions of the files
-#path, dirs, files = os.walk("images").next()
-#file_count = len(files)
-#file_count = file_count / 2 #a and b for each image
-#i = 1
+
 minNum = 1
 maxNum = 29
 for i in my_range(minNum,maxNum,1):
